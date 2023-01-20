001047b35a7d123bf7a43172ba06b880ede392f4
NatKhomich <yalapulya94@mail.ru>
2023-01-20 11:30:48 +0600

    task20

--- index.js
+++ index.js
@@ -18,4 +18,80 @@ var a = 9;
 var myFirstName = 'Natalia';
 var myLastName = 'Khomich';
 
+//Задание 6
+var a;
+var b;
+var c;
 
+a = 5;
+b = 10;
+c = "I am a"
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Задание 6
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Задание 7
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Задание 8
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact);
+
+//Задание 9, 10, 11, 12
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+//Задание 13
+let myVar = 87;
+myVar++;
+
+//Задание 14
+let myVar = 11;
+myVar--;
+
+//Задание 15
+const ourDecimal = 5.7;
+const myDecimal = 5.7;
+
+//Задание 16
+const product = 2.0 * 2.5;
+
+//Задание 17
+const quotient = 4.4 / 2.0;
+
+//Задание 18
+const remainder = 11%3;
+
+//Задание 19 
+let a = 3;
+let b = 17;
+let c = 12;
+
+a+=12;
+b+=9;
+c+=7;
+
+//Задание 20
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
