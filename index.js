68d68ffae994ed3a7dcbb7f9b885dcc62f1619f2
NatKhomich <yalapulya94@mail.ru>
2023-01-20 10:31:09 +0600

    new task

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-//Задача 1
+//Задание 1
 var myName;
 
-//Задача 2
-// Setup
+//Задание 2
 var a;
 a = 7;
+
+//Задание 3
+var a;
+a = 7;
+var b;
+b = a;
+
+//Задание 4
+var a = 9;
+
+//Задание 5
+var myFirstName = 'Natalia';
+var myLastName = 'Khomich';
 
 
