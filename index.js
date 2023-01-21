a254366e8eb839501f2ba8b23a7025e189d2f152
NatKhomich <yalapulya94@mail.ru>
2023-01-21 11:28:28 +0600

    new task

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//Задание 1
+//
 var myName;
-//Задание 2
+//
 var a;
 a = 7;
-//Задание 3
+//
 var a;
 a = 7;
 var b;
 b = a;
-//Задание 4
+//
 var a = 9;
-//Задание 5
+//
 var myFirstName = "Natalia";
 var myLastName = "Khomich";
-//Задание 6
+//
 var a;
 var b;
 var c;
@@ -23,80 +23,90 @@ c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
-//Задание 6
+//
 var studlyCapVar;
 var properCamelCase;
 var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
-//Задание 7
+//
 let catName = "Oliver";
 let catSound = "Meow!";
-//Задание 8
+//
 const FCC = "freeCodeCamp";
 let fact = "is cool!";
 fact = "is awesome!";
 console.log(FCC, fact);
-//Задание 9, 10, 11, 12
+//
 const sum = 10 + 10;
 const difference = 45 - 33;
 const productOne = 8 * 10;
 const quotientOne = 66 / 33;
-//Задание 13
+//
 let myVari = 87;
 myVar++;
-//Задание 14
+//
 let myVar = 11;
 myVar--;
-//Задание 15
+//
 const ourDecimal = 5.7;
 const myDecimal = 5.7;
-//Задание 16
+//
 const product = 2.0 * 2.5;
-//Задание 17
+//
 const quotient = 4.4 / 2.0;
-//Задание 18
+//
 const remainder = 11 % 3;
-//Задание 19
+//
 let a = 3;
 let b = 17;
 let c = 12;
 a += 12;
 b += 9;
 c += 7;
-//Задание 20
+//
 let a = 11;
 let b = 9;
 let c = 3;
 a -= 6;
 b -= 15;
 c -= 1;
-//Задание 21
+//
 let a = 5;
 let b = 12;
 let c = 4.6;
 a *= 5;
 b *= 3;
 c *= 10;
-//Задание 22
+//
 let a = 48;
 let b = 108;
 let c = 33;
 a /= 12;
 b /= 4;
 c /= 11;
-//Задание 23
+//
 const myStr = 'I am a "double quoted" string inside "double quotes".'; 
-//Задание 24, 25
+//
 const myStrN = 'This is the start. '+ 'This is the end.';
-let myStr = 'This is the first sentence. ';
+let myN = 'This is the first sentence. ';
 myStr += 'This is the second sentence.'
-//Задание 26
+//
 let lastNameLength = 0;
 const lastName = "Lovelace";
 lastNameLength = lastName;
 lastNameLength=lastName.length;
 
-firstLetterOfLastName = lastName; // Change this line
+firstLetterOfLastName = lastName; 
 firstLetterOfLastName = lastName[0]; 
+//
+let myStr = "Jello World";
+myStr = "Hello World"; 
+//
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
+//
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length-1];
+//
