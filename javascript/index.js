b1408382007fb302635cb0c28331235333e38333
NatKhomich <yalapulya94@mail.ru>
2023-01-26 13:19:26 +0600

    task

--- javascript/index.js
+++ javascript/index.js
@@ -346,3 +346,22 @@ function caseInSwitch(val) {
 }
 caseInSwitch(1);
 //
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+  return answer;
+}
+switchOfStuff(1);
+//
