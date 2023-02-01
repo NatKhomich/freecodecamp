e2d2f2a14f537cbcbb5587f0db6e3d6c2da9e806
NatKhomich <yalapulya94@mail.ru>
2023-02-01 13:13:42 +0600

    task

--- javascript/index.js
+++ javascript/index.js
@@ -522,4 +522,15 @@ const myMusic = [
   },
 ];
 //
-
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+const gloveBoxContents = myStorage.car.inside["glove box"];
